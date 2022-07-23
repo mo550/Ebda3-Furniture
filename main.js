ff766581edf022284904eb9bb81d50d79597e162
Mohamed Elsayed <mohamed.elsayed1420@icloud.com>
2022-07-23 22:44:11 +0200

    Update

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ let cartDOM = document.querySelector('.cart');
 let cartBtn = document.querySelector('.cart-btn');
 let closeCartBtn = document.querySelector('.close-cart');
 
+
+// Showing Cart
 cartBtn.addEventListener('click', () => {
   cartDOM.classList.add('showCart');
-  cartOverlay.classList.add('transparentBcg');
+  cartOverlay.classList.add('transparentBg');
 });
 
+// Hidding Cart
 closeCartBtn.addEventListener('click', () => {
   cartDOM.classList.remove('showCart');
-  cartOverlay.classList.remove('transparentBcg');
+  cartOverlay.classList.remove('transparentBg');
 });
 /* ------------------------------------------------------------------ */
 
@@ -25,10 +28,33 @@ const productsDOM = document.querySelector('.products-center');
 let cart = [];
 
 // Getting The Products
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+
+      // Destructuring JSON Object To Make It Easy
+      products = products.map((item) => {
+        let {title, price} = item.fields;
+        let {id} = item.sys;
+        let image = item.field.image.fields.file.url;
+      });
+      console.log(products);
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // Display Products
-class UI {}
+class UI {
+  displayProducts(products) {
+
+  }
+}
 
 // Local Storage
 class Storage {}
@@ -36,4 +62,10 @@ class Storage {}
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
+
+  // Get all products
+  products.getProducts().then((products) => {
+    ui.displayProducts(products)
+  }
+  );
 });
